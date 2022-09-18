ee503ffa239ffbfeffef2bd92d2cee49a794a728
Manish Khilari <mkhilari@Manishs-MacBook-Air.local>
2022-09-19 09:23:58 +1000

    Property code updated to handle the Domain API method getProperty rate limit of 500 calls per day

--- scripts/getDomainProperties.py
+++ scripts/getDomainProperties.py
@@ -40,7 +40,7 @@ def main():
     outputPropertyFileName = f"../data/raw/properties.csv" 
     print(f"Writing the saved properties to the following location \n") 
     print(f"{outputPropertyFileName} \n") 
-    outputPropertyFile = open(outputPropertyFileName, mode = "w") 
+    outputPropertyFile = open(outputPropertyFileName, mode = "a") 
 
     # Get the property column names 
     propertyColNames = [
@@ -61,64 +61,76 @@ def main():
 
     propertyHeader = propertyHeader[:-1] 
 
-    outputPropertyFile.write(f"{propertyHeader}\n") 
+    if (os.path.getsize(outputPropertyFileName) == 0): 
+        
+        outputPropertyFile.write(f"{propertyHeader}\n") 
 
     # Get the given properties 
     numDomainPropertyRequests = 0 
 
     for propertyID in propertyIDs: 
 
-        try: 
+        propertyJSONFileName = f"{propertiesFolder}/{propertyID}.json" 
 
-            print(f"Requesting the following property ID \n") 
-            print(f"{propertyID} \n") 
+        if (os.path.exists(propertyJSONFileName)): 
 
-            property = aPropertyReader.getProperty(propertyID) 
-            numDomainPropertyRequests += 1 
-
-            # Save the requested property 
-            propertyJSONFileName = f"{propertiesFolder}/{propertyID}.json" 
-            print(f"Writing the requested property to the following location \n") 
+            print(f"The given property is already stored at the following location \n") 
             print(f"{propertyJSONFileName} \n") 
-            propertyJSONFile = open(
-                propertyJSONFileName, mode = "w", encoding = "utf-8") 
-            json.dump(property, fp = propertyJSONFile, 
-            ensure_ascii = False, indent = 4) 
-            propertyJSONFile.close() 
 
-            # Get the property line 
-            propertyCanonicalUrl = property["canonicalUrl"] 
-            propertyLat = property["addressCoordinate"]["lat"] 
-            propertyLon = property["addressCoordinate"]["lon"] 
-            propertyAddressID = property["addressId"] 
-            propertyBathrooms = property["bathrooms"] 
-            propertyBedrooms = property["bedrooms"] 
-            propertyCarSpaces = property["carSpaces"] 
-            propertyCreated = property["created"] 
-            propertyFeatures = " ".join(property["features"]) 
-            propertyIsResidential = property["isResidential"] 
-            propertyPhotos = " ".join(
-                [photo["fullUrl"] for photo in property["photos"]]
-            ) 
-            propertyPlanNumber = property["planNumber"] 
-            propertyPostcode = property["postcode"] 
-            propertyCategory = property["propertyCategory"] 
-            propertyState = property["state"] 
-            propertyStatus = property["status"] 
-            propertyStreetAddress = property["streetAddress"] 
-            propertyStreetName = property["streetName"] 
-            propertyStreetNumber = property["streetNumber"] 
-            propertyStreetTypeLong = property["streetTypeLong"] 
-            propertySuburb = property["suburb"] 
+            continue 
 
-            propertyLine = f"{propertyID},{propertyCanonicalUrl},{propertyLat},{propertyLon},{propertyAddressID},{propertyBathrooms},{propertyBedrooms},{propertyCarSpaces},{propertyCreated},{propertyFeatures},{propertyIsResidential},{propertyPhotos},{propertyPlanNumber},{propertyPostcode},{propertyCategory},{propertyState},{propertyStatus},{propertyStreetAddress},{propertyStreetName},{propertyStreetNumber},{propertyStreetTypeLong},{propertySuburb}" 
+        print(f"Requesting the following property ID \n") 
+        print(f"{propertyID} \n") 
 
-            outputPropertyFile.write(f"{propertyLine}\n") 
-        except Exception as e: 
+        property = aPropertyReader.getProperty(propertyID) 
+        numDomainPropertyRequests += 1 
 
+        if "type" in property: 
+
+            print(f"{property} \n\n") 
             print(f"The maximum number of property requests have been exceeded \n") 
             print(f"Domain API maximum number of property requests per day: {MAX_DOMAIN_PROPERTY_REQUESTS_PER_DAY} \n") 
             print(f"Number of requests completed: {numDomainPropertyRequests} \n") 
+
+            break 
+
+        # Save the requested property 
+        print(f"Writing the requested property to the following location \n") 
+        print(f"{propertyJSONFileName} \n") 
+        propertyJSONFile = open(
+            propertyJSONFileName, mode = "w", encoding = "utf-8") 
+        json.dump(property, fp = propertyJSONFile, 
+        ensure_ascii = False, indent = 4) 
+        propertyJSONFile.close() 
+
+        # Get the property line 
+        propertyCanonicalUrl = property["canonicalUrl"] 
+        propertyLat = property["addressCoordinate"]["lat"] 
+        propertyLon = property["addressCoordinate"]["lon"] 
+        propertyAddressID = property["addressId"] 
+        propertyBathrooms = property["bathrooms"] 
+        propertyBedrooms = property["bedrooms"] 
+        propertyCarSpaces = property["carSpaces"] 
+        propertyCreated = property["created"] 
+        propertyFeatures = " ".join(property["features"]) 
+        propertyIsResidential = property["isResidential"] 
+        propertyPhotos = " ".join(
+            [photo["fullUrl"] for photo in property["photos"]]
+        ) 
+        propertyPlanNumber = property["planNumber"] 
+        propertyPostcode = property["postcode"] 
+        propertyCategory = property["propertyCategory"] 
+        propertyState = property["state"] 
+        propertyStatus = property["status"] 
+        propertyStreetAddress = property["streetAddress"] 
+        propertyStreetName = property["streetName"] 
+        propertyStreetNumber = property["streetNumber"] 
+        propertyStreetTypeLong = property["streetTypeLong"] 
+        propertySuburb = property["suburb"] 
+
+        propertyLine = f"{propertyID},{propertyCanonicalUrl},{propertyLat},{propertyLon},{propertyAddressID},{propertyBathrooms},{propertyBedrooms},{propertyCarSpaces},{propertyCreated},{propertyFeatures},{propertyIsResidential},{propertyPhotos},{propertyPlanNumber},{propertyPostcode},{propertyCategory},{propertyState},{propertyStatus},{propertyStreetAddress},{propertyStreetName},{propertyStreetNumber},{propertyStreetTypeLong},{propertySuburb}" 
+
+        outputPropertyFile.write(f"{propertyLine}\n") 
         
     # Close the property ID file after completion 
     propertyIDFile.close() 
